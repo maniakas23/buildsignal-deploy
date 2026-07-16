8bb43e4467df7f16e883fde79fbd7c093bb4f6c4
maniakas23 <kemsoftball@icloud.com>
2026-07-16 16:27:20 -0400

    Resolve merge: use Copilot's main.tsx with tRPC + React Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { queryClient, trpc, trpcClient } from "./lib/trpc";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </trpc.Provider>
+  </StrictMode>,
 );
