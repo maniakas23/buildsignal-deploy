a56d8d82f9e0239c12ac90032188289c6f2ef42a
maniakas23 <kemsoftball@icloud.com>
2026-07-18 16:35:30 -0400

    Fix: Remove SkipNavLink import that doesn't exist on GitHub

--- src/components/ui-custom/Layout.tsx
+++ src/components/ui-custom/Layout.tsx
@@ -5,7 +5,7 @@ import SurgeAlertModal from './SurgeAlertModal';
 import BetaFeedback from './BetaFeedback';
 import OnboardingModal from './Onboarding';
 import DemoBadge from './DemoBadge';
-import { SkipNavLink, MainContent } from './SkipNav';
+import { MainContent } from './SkipNav';
 
 interface LayoutProps {
   children: ReactNode;
@@ -14,7 +14,6 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className="min-h-screen bg-canvas pb-16 md:pb-0">
-      <SkipNavLink />
       <Navbar />
       <div className="flex justify-center pt-2">
         <DemoBadge />
