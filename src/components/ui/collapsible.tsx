ca02d23d20e6dddfd674ae191ca40c1bef1fb4e3
maniakas23 <kemsoftball@icloud.com>
2026-08-17 18:32:01 -0400

    fix: reconcile first-customer production closeout

--- src/components/ui/collapsible.tsx
+++ src/components/ui/collapsible.tsx
@@ -1,1 +1,7 @@
-export const Collapsible = () => null; export const CollapsibleTrigger = () => null; export const CollapsibleContent = () => null;
+import * as CollapsiblePrimitive from "@radix-ui/react-collapsible";
+
+const Collapsible = CollapsiblePrimitive.Root;
+const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger;
+const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent;
+
+export { Collapsible, CollapsibleTrigger, CollapsibleContent };
